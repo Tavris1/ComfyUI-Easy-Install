e49e3abd4c4ab2bd22766f52ca8c792a948ef1f5
ivo <36665424+Tavris1@users.noreply.github.com>
2025-07-24 14:05:43 +0300

    Update ComfyUI-EZi-Nunchaku.bat

--- ComfyUI-EZi-Nunchaku.bat
+++ ComfyUI-EZi-Nunchaku.bat
@@ -10,7 +10,8 @@ set "PIPargs=--no-cache-dir --no-warn-script-location --timeout=1000 --retries 2
 set "CURLargs=--retry 200 --retry-all-errors"
 
 :: Set local path only (temporarily) ::
-set path=
+for /f "delims=" %%G in ('cmd /c "where git.exe 2>nul"') do (set "GIT_PATH=%%~dpG")
+set path=%GIT_PATH%
 if exist %windir%\System32 set path=%PATH%;%windir%\System32
 if exist %windir%\System32\WindowsPowerShell\v1.0 set path=%PATH%;%windir%\System32\WindowsPowerShell\v1.0
 if exist %localappdata%\Microsoft\WindowsApps set path=%PATH%;%localappdata%\Microsoft\WindowsApps
