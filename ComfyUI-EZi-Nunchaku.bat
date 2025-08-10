9926f9aba3c7e020e152dae3c8814aebb2b8b791
ivo <36665424+Tavris1@users.noreply.github.com>
2025-08-10 09:46:41 +0300

    Add files via upload

--- ComfyUI-EZi-Nunchaku.bat
+++ ComfyUI-EZi-Nunchaku.bat
@@ -192,7 +192,7 @@ Echo Scripts>> python311._pth
 Echo # import site>> python311._pth
 
 .\python.exe -I get-pip.py %PIPargs%
-.\python.exe -I -m pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128 %PIPargs%
+.\python.exe -I -m pip install torch==2.8.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128 %PIPargs%
 .\python.exe -I -m pip install pygit2 %PIPargs%
 cd ..\ComfyUI
 ..\python_embeded\python.exe -I -m pip install -r requirements.txt %PIPargs%
