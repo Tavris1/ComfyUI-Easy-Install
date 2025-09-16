323117b309864688da7c182116a98c59f6e20872
VenimK <venimkmac@gmail.com>
2025-09-16 21:12:59 +0200

    feat: add Git LFS smudge skip flag around LTXVideo node installation

--- ComfyUI-Easy-Install-Linux.sh
+++ ComfyUI-Easy-Install-Linux.sh
@@ -270,7 +270,9 @@ get_node "https://github.com/smthemex/ComfyUI_Sonic" "ComfyUI_Sonic"
 get_node "https://github.com/welltop-cn/ComfyUI-TeaCache" "teacache"
 get_node "https://github.com/kk8bit/KayTool" "kaytool"
 get_node "https://github.com/shiimizu/ComfyUI-TiledDiffusion" "ComfyUI-TiledDiffusion"
+set GIT_LFS_SKIP_SMUDGE=1
 # get_node "https://github.com/Lightricks/ComfyUI-LTXVideo" "ComfyUI-LTXVideo"
+set GIT_LFS_SKIP_SMUDGE=
 get_node "https://github.com/kijai/ComfyUI-KJNodes" "comfyui-kjnodes"
 get_node "https://github.com/kijai/ComfyUI-WanVideoWrapper" "ComfyUI-WanVideoWrapper"
 
