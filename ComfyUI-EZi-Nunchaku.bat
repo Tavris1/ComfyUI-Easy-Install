4ddb6666401dd447d5c3f4301471feb371efb766
ivo <36665424+Tavris1@users.noreply.github.com>
2025-09-16 05:25:12 +0200

    Add files via upload

--- ComfyUI-EZi-Nunchaku.bat
+++ ComfyUI-EZi-Nunchaku.bat
@@ -98,9 +98,9 @@ call :get_node https://github.com/smthemex/ComfyUI_Sonic						ComfyUI_Sonic
 call :get_node https://github.com/welltop-cn/ComfyUI-TeaCache					teacache
 call :get_node https://github.com/kk8bit/KayTool								kaytool
 call :get_node https://github.com/shiimizu/ComfyUI-TiledDiffusion				ComfyUI-TiledDiffusion
-set GIT_LFS_SKIP_SMUDGE=1
-call :get_node https://github.com/Lightricks/ComfyUI-LTXVideo					ComfyUI-LTXVideo
-set GIT_LFS_SKIP_SMUDGE=
+REM set GIT_LFS_SKIP_SMUDGE=1
+REM call :get_node https://github.com/Lightricks/ComfyUI-LTXVideo					ComfyUI-LTXVideo
+REM set GIT_LFS_SKIP_SMUDGE=
 call :get_node https://github.com/kijai/ComfyUI-KJNodes							comfyui-kjnodes
 call :get_node https://github.com/kijai/ComfyUI-WanVideoWrapper					ComfyUI-WanVideoWrapper
 echo %green%::::::::::::::: Installing%yellow% ComfyUI-nunchaku %green%:::::::::::::::%reset%
