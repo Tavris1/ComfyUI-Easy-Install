ad111a9471fa34e3cbab053fb3cee0a29f19baa6
VenimK <venimkmac@gmail.com>
2025-12-05 06:37:35 +0100

    v1.71.2 (Ep71)

--- ComfyUI-Easy-Install.sh
+++ ComfyUI-Easy-Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Title: ComfyUI-Easy-Install NEXT by ivo v1.72. (Ep70)
+# Title: ComfyUI-Easy-Install NEXT by ivo v1.71.2 (Ep71)
 # Pixaroma Community Edition
 # macOS and Linux conversion
 
