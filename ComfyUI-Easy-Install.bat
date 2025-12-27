5703b273c2ae1f326c4c0073d035656bcb7196b2
ivo <36665424+Tavris1@users.noreply.github.com>
2025-12-27 14:59:52 +0200

    v1.73.1 (Ep73)

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @Echo off&&cd /D %~dp0
-Title ComfyUI-Easy-Install NEXT by ivo v1.73.0 (Ep73)
+Title ComfyUI-Easy-Install NEXT by ivo v1.73.1 (Ep73)
 :: Pixaroma Community Edition ::
 
 :: Set the Python version here (3.11 or 3.12 only) ::
