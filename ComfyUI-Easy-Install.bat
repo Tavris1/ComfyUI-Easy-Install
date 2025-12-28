47256fee4b773804e8d7f3104e06372a80940e46
ivo <36665424+Tavris1@users.noreply.github.com>
2025-12-28 11:31:52 +0200

    v2.01.0

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,39 +1,27 @@
 @Echo off&&cd /D %~dp0
-Title ComfyUI-Easy-Install NEXT by ivo v1.73.1 (Ep73)
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.01.0"
+Title %CEI_Title%
 :: Pixaroma Community Edition ::
-
-:: Set the Python version here (3.11 or 3.12 only) ::
-set "PYTHON_VERSION=3.12"
 
 :: Set colors ::
 call :set_colors
-
-:: Check the Python version ::
-if not "%PYTHON_VERSION%"=="3.11" if not "%PYTHON_VERSION%"=="3.12" (
-    echo.
-    echo %warning%WARNING: %red%Python %PYTHON_VERSION% is not supported. %green%Supported versions: 3.11, 3.12%reset%
-    echo.
-    echo %red%::::::::::::::: Press any key to exit%reset%&Pause>nul
-    exit
-)
 
 :: Set Ignoring Large File Storage ::
 set GIT_LFS_SKIP_SMUDGE=1
 
 :: Set arguments ::
-set "PIPargs=--no-cache-dir --no-warn-script-location --timeout=1000 --retries 200"
-set "CURLargs=--retry 200 --retry-all-errors"
+set "PIPargs=--no-cache-dir --no-warn-script-location --timeout=1000 --retries 10"
 set "UVargs=--no-cache --link-mode=copy"
 
 :: Set local path only (temporarily) ::
 for /f "delims=" %%G in ('cmd /c "where.exe git.exe 2>nul"') do (set "GIT_PATH=%%~dpG")
 set "path=%GIT_PATH%"
-if exist "%windir%\system32" set "path=%PATH%;%windir%\System32"
-if exist "%windir%\system32\WindowsPowerShell\v1.0" set "path=%PATH%;%windir%\system32\WindowsPowerShell\v1.0"
+if exist "%windir%\System32" set "path=%PATH%;%windir%\System32"
+if exist "%windir%\System32\WindowsPowerShell\v1.0" set "path=%PATH%;%windir%\System32\WindowsPowerShell\v1.0"
 if exist "%localappdata%\Microsoft\WindowsApps" set "path=%PATH%;%localappdata%\Microsoft\WindowsApps"
 
 :: Check for Existing ComfyUI Folder ::
-if exist ComfyUI-Easy-Install (
+if exist ComfyUI-Easy-Install if exist "ComfyUI-Easy-Install" (
 	echo %warning%WARNING:%reset% '%bold%ComfyUI-Easy-Install%reset%' folder already exists!
 	echo %green%Move this file to another folder and run it again.%reset%
 	echo Press any key to Exit...&Pause>nul
@@ -41,8 +29,8 @@ if exist ComfyUI-Easy-Install (
 )
 
 :: Check for Existing Helper-CEI ::
-set "HLPR_NAME=Helper-CEI-NEXT.zip"
-if not exist %HLPR_NAME% (
+set "HLPR_NAME=Helper-CEI.zip"
+if not exist "%HLPR_NAME%" (
 	echo %warning%WARNING:%reset% '%bold%%HLPR_NAME%%reset%' not exists!
 	echo %green%Unzip the entire package and try again.%reset%
 	echo Press any key to Exit...&Pause>nul
@@ -87,8 +75,8 @@ Echo %gitversion% | findstr /C:"version">nul&&(
 )
 
 :: System folder? ::
-md ComfyUI-Easy-Install
-if not exist ComfyUI-Easy-Install (
+md "ComfyUI-Easy-Install"
+if not exist "ComfyUI-Easy-Install" (
 	cls
 	echo %warning%WARNING:%reset% Cannot create folder %yellow%ComfyUI-Easy-Install%reset%
 	echo Make sure you are NOT using system folders like %yellow%Program Files, Windows%reset% or system root %yellow%C:\%reset%
@@ -96,95 +84,77 @@ if not exist ComfyUI-Easy-Install (
 	echo Press any key to Exit...&Pause>nul
 	exit /b
 )
-cd ComfyUI-Easy-Install
+cd "ComfyUI-Easy-Install"
 
 :: Install ComfyUI ::
 call :install_comfyui
 
 echo %green%::::::::::::::: %yellow%Pre-installation of required modules%green% :::::::::::::::%reset%
 echo.
-:: Install llama-cpp-python for Searge ::
-if "%PYTHON_VERSION%"=="3.12" (.\python_embeded\python.exe -I -m uv pip install https://github.com/abetlen/llama-cpp-python/releases/download/v0.3.4-cu124/llama_cpp_python-0.3.4-cp312-cp312-win_amd64.whl %UVargs%)
+.\python_embeded\python.exe -I -m uv pip install scikit-build-core %UVargs%
+.\python_embeded\python.exe -I -m uv pip install onnxruntime-gpu %UVargs%
+.\python_embeded\python.exe -I -m uv pip install onnx %UVargs%
+.\python_embeded\python.exe -I -m uv pip install flet %UVargs%
+.\python_embeded\python.exe -I -m uv pip install https://github.com/JamePeng/llama-cpp-python/releases/download/v0.3.18-cu130-Basic-win-20251223/llama_cpp_python-0.3.18-cp312-cp312-win_amd64.whl %UVargs%
 :: Install working version of stringzilla (damn it) ::
 .\python_embeded\python.exe -I -m uv pip install stringzilla==3.12.6 %UVargs%
-:: Install scikit-build-core ::
-.\python_embeded\python.exe -I -m uv pip install scikit-build-core %UVargs%
 echo.
 
 :: Install Pixaroma's Related Nodes ::
 call :get_node https://github.com/Comfy-Org/ComfyUI-Manager						comfyui-manager
-call :get_node https://github.com/WASasquatch/was-node-suite-comfyui			was-node-suite-comfyui
 call :get_node https://github.com/yolain/ComfyUI-Easy-Use						ComfyUI-Easy-Use
 call :get_node https://github.com/Fannovel16/comfyui_controlnet_aux				comfyui_controlnet_aux
-call :get_node https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes			ComfyUI_Comfyroll_CustomNodes
-call :get_node https://github.com/crystian/ComfyUI-Crystools					ComfyUI-Crystools
 call :get_node https://github.com/rgthree/rgthree-comfy							rgthree-comfy
-call :get_node https://github.com/city96/ComfyUI-GGUF							ComfyUI-GGUF
-call :get_node https://github.com/kijai/ComfyUI-Florence2						ComfyUI-Florence2
-call :get_node https://github.com/SeargeDP/ComfyUI_Searge_LLM					ComfyUI_Searge_LLM
-call :get_node https://github.com/gseth/ControlAltAI-Nodes						controlaltai-nodes
-call :get_node https://github.com/stavsap/comfyui-ollama						comfyui-ollama
 call :get_node https://github.com/MohammadAboulEla/ComfyUI-iTools				comfyui-itools
-call :get_node https://github.com/spinagon/ComfyUI-seamless-tiling				comfyui-seamless-tiling
+call :get_node https://github.com/city96/ComfyUI-GGUF							ComfyUI-GGUF
+call :get_node https://github.com/gseth/ControlAltAI-Nodes						controlaltai-nodes
 call :get_node https://github.com/lquesada/ComfyUI-Inpaint-CropAndStitch		comfyui-inpaint-cropandstitch
-call :get_node https://github.com/Lerc/canvas_tab								canvas_tab
-call :get_node https://github.com/1038lab/ComfyUI-OmniGen						comfyui-omnigen
-call :get_node https://github.com/john-mnz/ComfyUI-Inspyrenet-Rembg				comfyui-inspyrenet-rembg
-call :get_node https://github.com/kaibioinfo/ComfyUI_AdvancedRefluxControl		ComfyUI_AdvancedRefluxControl
+call :get_node https://github.com/1038lab/ComfyUI-RMBG							comfyui-rmbg
 call :get_node https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite			comfyui-videohelpersuite
-call :get_node https://github.com/PowerHouseMan/ComfyUI-AdvancedLivePortrait	comfyui-advancedliveportrait
-call :get_node https://github.com/Yanick112/ComfyUI-ToSVG						ComfyUI-ToSVG
-call :get_node https://github.com/stavsap/comfyui-kokoro						comfyui-kokoro
-call :get_node https://github.com/CY-CHENYUE/ComfyUI-Janus-Pro					janus-pro
-call :get_node https://github.com/smthemex/ComfyUI_Sonic						ComfyUI_Sonic
 call :get_node https://github.com/welltop-cn/ComfyUI-TeaCache					teacache
-call :get_node https://github.com/kk8bit/KayTool								kaytool
 call :get_node https://github.com/shiimizu/ComfyUI-TiledDiffusion				ComfyUI-TiledDiffusion
-call :get_node https://github.com/Lightricks/ComfyUI-LTXVideo					ComfyUI-LTXVideo
 call :get_node https://github.com/kijai/ComfyUI-KJNodes							comfyui-kjnodes
 call :get_node https://github.com/kijai/ComfyUI-WanVideoWrapper					ComfyUI-WanVideoWrapper
-call :get_node https://github.com/Enemyx-net/VibeVoice-ComfyUI					VibeVoice-ComfyUI
-call :get_node https://github.com/ussoewwin/ComfyUI-QwenImageLoraLoader			ComfyUI-QwenImageLoraLoader
 call :get_node https://github.com/1038lab/ComfyUI-QwenVL						ComfyUI-QwenVL
-
-echo %green%::::::::::::::: Installing %yellow%Required Modules%green% :::::::::::::::%reset%
-echo.
-
-:: Install pylatexenc for kokoro ::
-.\python_embeded\python.exe -I -m uv pip install https://www.piwheels.org/simple/pylatexenc/pylatexenc-3.0a32-py3-none-any.whl %UVargs%
-:: Install onnxruntime ::
-.\python_embeded\python.exe -I -m uv pip install onnxruntime-gpu %UVargs%
-.\python_embeded\python.exe -I -m uv pip install onnx %UVargs%
-:: Install flet for REMBG ::
-.\python_embeded\python.exe -I -m uv pip install flet %UVargs%
-:: Install ffmpeg ::
-.\python_embeded\python.exe -I -m uv pip install python-ffmpeg %UVargs%
 
 :: Extracting helper folders ::
 cd ..\
-tar.exe -xf .\%HLPR_NAME%
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive -LiteralPath '%HLPR_NAME%' -DestinationPath '.' -Force"
 cd ComfyUI-Easy-Install
-if "%PYTHON_VERSION%"=="3.11" (xcopy "python_embeded_3.11\*" "python_embeded\" /E /Y /I /Q)
-if "%PYTHON_VERSION%"=="3.12" (xcopy "python_embeded_3.12\*" "python_embeded\" /E /Y /I /Q)
-if exist "python_embeded_3.11" rmdir /s /q "python_embeded_3.11"
-if exist "python_embeded_3.12" rmdir /s /q "python_embeded_3.12"
+
+if exist ".\Add-Ons\Tools\AutoRun.bat" (
+	pushd %cd%
+	call ".\Add-Ons\Tools\AutoRun.bat"
+	popd
+	Title %CEI_Title%
+	del  ".\Add-Ons\Tools\AutoRun.bat"
+)
 
 :: Installing Nunchaku from the Add-ons ::
-REM pushd %CD%&&echo.&&call Add-Ons\Nunchaku-NEXT.bat NoPause&&popd
+REM pushd %CD%&&echo.&&call Add-Ons\Nunchaku.bat NoPause&&popd
 :: Installing SageAttention from the Add-ons ::
-REM pushd %CD%&&echo.&&call Add-Ons\SageAttention-NEXT.bat NoPause&&popd
+REM pushd %CD%&&echo.&&call Add-Ons\SageAttention.bat NoPause&&popd
 :: Installing Insightface from the Add-ons ::
-REM pushd %CD%&&echo.&&call Add-Ons\Insightface-NEXT.bat NoPause&&popd
+REM pushd %CD%&&echo.&&call Add-Ons\Insightface.bat NoPause&&popd
 
 :: Capture the end time ::
 for /f "delims=" %%i in ('powershell -command "Get-Date -Format yyyy-MM-dd_HH:mm:ss"') do set end=%%i
-
 for /f "delims=" %%i in ('powershell -command "$s=[datetime]::ParseExact('%start%','yyyy-MM-dd_HH:mm:ss',$null); $e=[datetime]::ParseExact('%end%','yyyy-MM-dd_HH:mm:ss',$null); if($e -lt $s){$e=$e.AddDays(1)}; ($e-$s).TotalSeconds"') do set diff=%%i
 
+if not exist ".\ComfyUI\custom_nodes\.disabled" mkdir ".\ComfyUI\custom_nodes\.disabled"
+
 :: Create a shortcut on the desktop ::
-pushd %cd%
-call "ComfyUI\Desktop Shortcut.bat"
-popd
+if exist ".\Add-Ons\Tools\Helper-CEI\ComfyUI-EZi.ico" if exist ".\Start ComfyUI.bat" (
+	echo.
+	echo %green%:::::: Creating desktop shortcut to start ComfyUI ::::::%reset%
+	powershell -command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%USERPROFILE%\Desktop\ComfyUI-EZi.lnk'); $s.TargetPath='%cd%\Start ComfyUI.bat'; $s.WorkingDirectory='%cd%\'; $s.IconLocation='%cd%\Add-Ons\Tools\Helper-CEI\ComfyUI-EZi.ico'; $s.Save();"
+)
+
+if exist ".\Add-Ons\Tools\Helper-CEI\ComfyUI-EZi-output.ico" (
+	echo.
+	echo %green%:::::::: Creating ComfyUI output folder shortcut :::::::%reset%
+	powershell -command "$s=(New-Object -ComObject WScript.Shell).CreateShortcut('%USERPROFILE%\Desktop\ComfyUI-EZi output.lnk'); $s.TargetPath='%cd%\ComfyUI\output'; $s.WorkingDirectory='%cd%\'; $s.IconLocation='%cd%\Add-Ons\Tools\Helper-CEI\ComfyUI-EZi-output.ico, 0'; $s.Save();"
+)
 
 :: Final Messages ::
 echo.
@@ -226,35 +196,36 @@ goto :eof
 :: https://github.com/comfyanonymous/ComfyUI
 echo %green%::::::::::::::: Installing%yellow% ComfyUI %green%:::::::::::::::%reset%
 echo.
+
 git.exe clone https://github.com/comfyanonymous/ComfyUI ComfyUI
 
-if "%PYTHON_VERSION%"=="3.11" (set "PYTHON_VER=3.11.9")
-if "%PYTHON_VERSION%"=="3.12" (set "PYTHON_VER=3.12.10")
+:: Disable only CRL/OCSP checks for SSL ::
+powershell -Command "[System.Net.ServicePointManager]::CheckCertificateRevocationList = $false"
+
+:: Ignore SSL certificate errors ::
+REM powershell -Command "Add-Type @'using System.Net;using System.Security.Cryptography.X509Certificates;public class TrustAllCertsPolicy : ICertificatePolicy {public bool CheckValidationResult(ServicePoint srvPoint,X509Certificate certificate,WebRequest request,int certificateProblem){return true;}}'@;[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy"
 
 md python_embeded&&cd python_embeded
-curl.exe -OL https://www.python.org/ftp/python/%PYTHON_VER%/python-%PYTHON_VER%-embed-amd64.zip --ssl-no-revoke %CURLargs%
-tar.exe -xf python-%PYTHON_VER%-embed-amd64.zip
-erase python-%PYTHON_VER%-embed-amd64.zip
-curl.exe -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py --ssl-no-revoke %CURLargs%
+REM curl.exe -OL https://www.python.org/ftp/python/3.12.10/python-3.12.10-embed-amd64.zip --ssl-no-revoke %CURLargs%
+powershell.exe -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.12.10/python-3.12.10-embed-amd64.zip' -OutFile 'python-3.12.10-embed-amd64.zip' -UseBasicParsing"
+tar.exe -xf python-3.12.10-embed-amd64.zip
+REM powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive -LiteralPath 'python-3.12.10-embed-amd64.zip' -DestinationPath '.' -Force"
+erase python-3.12.10-embed-amd64.zip
+REM curl.exe -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py --ssl-no-revoke %CURLargs%
+powershell.exe -Command "Invoke-WebRequest -Uri 'https://bootstrap.pypa.io/get-pip.py' -OutFile 'get-pip.py' -UseBasicParsing"
 
-Echo ../ComfyUI> python311._pth
 Echo ../ComfyUI> python312._pth
-Echo python311.zip>> python311._pth
 Echo python312.zip>> python312._pth
-Echo .>> python311._pth
 Echo .>> python312._pth
-Echo Lib/site-packages>> python311._pth
 Echo Lib/site-packages>> python312._pth
-Echo Lib>> python311._pth
 Echo Lib>> python312._pth
-Echo Scripts>> python311._pth
 Echo Scripts>> python312._pth
-Echo # import site>> python311._pth
 Echo # import site>> python312._pth
 
 .\python.exe -I get-pip.py %PIPargs%
 .\python.exe -I -m pip install uv==0.9.7 %PIPargs%
-.\python.exe -I -m pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu128 %PIPargs%
+REM .\python.exe -I -m pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu128 %PIPargs%
+.\python.exe -I -m pip install torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1 --index-url https://download.pytorch.org/whl/cu130 %PIPargs%
 .\python.exe -I -m uv pip install pygit2 %UVargs%
 cd ..\ComfyUI
 ..\python_embeded\python.exe -I -m uv pip install -r requirements.txt %UVargs%
@@ -277,10 +248,10 @@ if exist ".\ComfyUI\custom_nodes\%git_folder%\requirements.txt" (
     )
 )
 
-if exist .\ComfyUI\custom_nodes\%git_folder%\install.py (
+if exist ".\ComfyUI\custom_nodes\%git_folder%\install.py" (
     for %%F in (".\ComfyUI\custom_nodes\%git_folder%\install.py") do set filesize=%%~zF
     if not !filesize! equ 0 (
-	.\python_embeded\python.exe -I .\ComfyUI\custom_nodes\%git_folder%\install.py
+	.\python_embeded\python.exe -I ".\ComfyUI\custom_nodes\%git_folder%\install.py"
 	)
 )
 endlocal
