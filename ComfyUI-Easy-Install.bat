1ded039d4b83bb80e338229af096becd821d65db
ivo <36665424+Tavris1@users.noreply.github.com>
2025-12-30 22:24:54 +0200

    v2.01.1

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @Echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.01.0"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.01.1"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
