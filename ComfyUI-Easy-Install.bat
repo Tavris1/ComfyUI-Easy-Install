a5773a60041f2f4d965f41a3786ef8a5b65984ef
ivo <36665424+Tavris1@users.noreply.github.com>
2026-01-28 16:14:25 +0100

    v2.02.5
    - Removed automatic pip and uv cache cleanup across all affected modules to avoid unexpected cache deletion

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @Echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.02.3"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.02.5"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
