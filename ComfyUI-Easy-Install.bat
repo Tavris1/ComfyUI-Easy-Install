720ee58e352f827aca680f6a635f37068152d31c
ivo <36665424+Tavris1@users.noreply.github.com>
2026-02-13 10:59:38 +0100

    v2.04.1
    - Improve stability in FlashAttention.bat when switching Torch versions

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @Echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.04.0"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.04.1"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
