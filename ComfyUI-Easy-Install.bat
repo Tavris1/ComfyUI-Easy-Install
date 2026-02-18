8466d5a811aa165415850b0a227d870aa72d24bf
ivo <36665424+Tavris1@users.noreply.github.com>
2026-02-18 20:57:24 +0100

    v2.05.1
    Added 'pooch' to Trellis2.bat

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @Echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.05.0"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.05.1"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
