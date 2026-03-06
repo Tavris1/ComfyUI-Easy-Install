eb08772d53376be8d7333254c8fa166d63beab0a
ivo <36665424+Tavris1@users.noreply.github.com>
2026-03-06 20:39:47 +0100

    v2.07.5
    chore: SoX installation: forcibly reset errorlevel to 0 to prevent false error propagation.

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.07.4"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.07.5"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
@@ -127,6 +127,7 @@ call :get_node https://github.com/flybirdxx/ComfyUI-Qwen-TTS				qwen3-tts-comfyu
 echo %green%::::::::::::::: %yellow%Installation/Updating SoX%green% :::::::::::::::%reset%
 echo.
 winget.exe install --id ChrisBagwell.SoX -e --accept-source-agreements --accept-package-agreements --silent
+cd .\
 echo.
 
 if not exist ".\ComfyUI\custom_nodes\.disabled" mkdir ".\ComfyUI\custom_nodes\.disabled"
