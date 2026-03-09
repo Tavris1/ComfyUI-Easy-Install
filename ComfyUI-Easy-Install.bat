58c1e65a9819d2c9da3384c3b8cf9fa72819c56f
ivo <36665424+Tavris1@users.noreply.github.com>
2026-03-09 12:19:58 +0100

    v2.07.6
    Trellis2 update

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.07.5"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.07.6"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
