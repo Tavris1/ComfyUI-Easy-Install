7b0880b79efa565d84cb88f909e25154f2469387
ivo <36665424+Tavris1@users.noreply.github.com>
2026-03-21 23:28:23 +0100

    v2.09.0
    Normalize `extra_model_paths.yaml` keys to lowercase in `Easy-Models-Linker.bat`

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.07.11"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.09.0"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
