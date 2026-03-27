c18e073ea00a63c2f002ededdb0d59326891d79a
ivo <36665424+Tavris1@users.noreply.github.com>
2026-03-27 10:34:30 +0100

    v2.10.1
    Fixed a bug when installing SageAttention 3

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.10.0"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.10.1"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
