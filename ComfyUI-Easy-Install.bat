e0025d7a035b23b8cd8b40b3021fa7cc0f3b16b4
ivo <36665424+Tavris1@users.noreply.github.com>
2026-04-04 12:47:36 +0200

    v2.11.1
    Removed chcp 65001 due to console display issues (UTF-8 code page)

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v2.11.0"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v2.11.1"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
