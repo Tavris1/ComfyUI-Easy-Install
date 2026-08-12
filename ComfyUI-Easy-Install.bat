76adc160b6809fedc8371ba8334e480011433fc9
ivo <36665424+Tavris1@users.noreply.github.com>
2026-08-12 11:53:39 +0200

    EZi Desktop v3.13.1

--- ComfyUI-Easy-Install.bat
+++ ComfyUI-Easy-Install.bat
@@ -1,5 +1,5 @@
 @echo off&&cd /D %~dp0
-set "CEI_Title=ComfyUI-Easy-Install by ivo v3.13.0"
+set "CEI_Title=ComfyUI-Easy-Install by ivo v3.13.1"
 Title %CEI_Title%
 :: Pixaroma Community Edition ::
 
