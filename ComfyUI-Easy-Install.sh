e8c42a4c5331019f996e94b609fe39bce438f5fa
VenimK <venimkmac@gmail.com>
2026-09-24 04:21:24 +0200

    v3.15.1

--- ComfyUI-Easy-Install.sh
+++ ComfyUI-Easy-Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Title ComfyUI-Easy-Install  NEXT by ivo 3.14.3
+# Title ComfyUI-Easy-Install  NEXT by ivo 3.15.1
 # Pixaroma Community Edition
 # macOS and Linux conversion by VenimK
 
